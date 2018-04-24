4ab902fe10b28f2d2416a5443b09e5a673a8845d
Himanshu Negi <ecoworld007@gmail.com>
2018-04-24 14:29:41 +0530

    using mongo module to insert todo and user

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,31 @@
+const MongoClient =  require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if(err){
+        return console.log('Unable to connect to mongodb server');
+    }
+    console.log('Connected to mongodb server');
+    let todoAppDb = client.db('TodoApp');
+    todoAppDb.collection('Todos').insertOne({
+        text: 'something to do',
+        completed: false
+    }, (err, res) => {
+        if(err){
+            return console.log('Unable to insert the todo');
+        }
+        console.log(JSON.stringify(res.ops, undefined, 2));
+    });
+
+    todoAppDb.collection('Users').insertOne({
+        name: 'Zoro',
+        age: '20',
+        location: 'East Blue'
+    }, (err, res) => {
+        if(err){
+            return console.log('Unable to insert the user');
+        }
+        console.log(JSON.stringify(res.ops, undefined, 2));
+    });
+
+    client.close();
+});
