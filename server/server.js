fea837e4bbe6e913639b198d68f397f0c7644e2c
Himanshu Negi <ecoworld007@gmail.com>
2018-04-26 17:11:10 +0530

    added delete /todos/:id route

--- server/server.js
+++ server/server.js
@@ -64,6 +64,20 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+app.delete('/todos/:id', (req, res) => {
+    if(!ObjectID.isValid(req.params.id)){
+        return res.status(404).send();
+    }
+    Todo.findByIdAndRemove(req.params.id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }, (err) => {
+        res.status(400).send();
+    });
+});
+
 app.listen(PORT, () => {
     console.log('Server listening to the port '+PORT);
 });
